0b57cedadddf1c7682028dae2ff4dbf76322c79d
ludmilla <l.carlier@live.nl>
2018-04-27 22:39:29 +0200

    first three functons working

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -9,6 +9,72 @@ const hero = {
         damage: 25
     }
 }
+
+const creature = {
+    name: 'Guy',
+    heroic: true,
+    inventory: [],
+    health: 100,
+    weapon: {
+        type: "Bow",
+        damage: 15
+    }
+}
+
+const attacker = {
+    name: 'Raawr',
+    heroic: true,
+    inventory: [],
+    health: 100,
+    weapon: {
+        type: "Sword",
+        damage: 10
+    }
+}
+
+const defender = {
+    name: 'Shield',
+    heroic: true,
+    inventory: [],
+    health: 100,
+    weapon: {
+        type: "shield",
+        damage: 5
+    }
+}
+
+var item =  {
+    type: "mesje",
+    damage: 2
+}
+
 // Game logic
+function rest(creature) {
+    creature.health += 10
+    return creature
+}
+
+// console.log(creature.health)
+// rest(creature) // WORKS
+// console.log(creature.health)
+
+function pickUpItem(creature, item) {
+    creature.inventory.push(item)
+    return creature
+}
+
+// console.log(creature.inventory)
+// pickUpItem(creature, item)//item is nog niet defined //WORKS
+// console.log(creature.inventory)
+
+function dealDamage(attacker, defender) {
+    defender.health -= attacker.weapon.damage
+    return defender
+}
+// console.log(defender.health)
+// dealDamage(attacker, defender) //WORKS
+// console.log(defender.health)
+
+
 
 // UI
